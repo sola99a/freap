8b3d1fe0bf6814cccf743d7d28cdd63f2f4c50c6
sola99a <solabidar@gmail.com>
2024-03-10 03:03:30 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+<button @click="get(32)">get</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  setup () {
+    function get(e){
+      console.log(e);
+axios.post('http://sollio.freehostia.com/freap/',e).then((res)=>{
+console.log(res.data);
+}).catch((er)=>{
+console.log(er);
+})
+    }
+
+    return {get}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
